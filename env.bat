307cad9774b92606de54d4b3891f223269c6bae0
wwcollins <williamwcollinsjr@gmail.com>
2023-04-05 16:37:49 -0500

    debug - issue with crash due with chatgpt engine

---
+++ env.bat
@@ -0,0 +1,8 @@
+echo %CD%
+rem cd C:\Code\stock_screener
+cd env/scripts
+call activate.bat
+cd ../..
+echo %CD%
+dir *.py
+
--- simple_chat_app_3.py
+++ simple_chat_app_3.py
@@ -187,16 +187,19 @@ import pandas as pd
 from io import StringIO
 
 uploaded_file = st.file_uploader("Choose a file to upload")
+
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
+    print('Read and write file as bytes'):
+    # bytes_data = uploaded_file.getvalue()
     st.write(bytes_data)
 
     # To convert to a string based IO:
+    print('convert to a string based IO')
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     st.write(stringio)
 
     # To read file as string:
+    print('To read file as string')
     string_data = stringio.read()
     st.write(string_data)
 
